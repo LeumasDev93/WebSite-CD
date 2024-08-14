0386ae8426c33165ca803d6e7254011ed088ded6
LeumasDev93 <cursosdrivecv22@gmail.com>
2024-08-14 20:58:27 -0100

    inprove code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,23 +5,23 @@ import { ThemeContext } from "@/context/ThemeContext";
 import Link from "next/link";
 
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCartShopping } from "react-icons/fa6"; // Verifique o ícone de compras, o correto seria 'fa6' se estivesse disponível como mencionado.
 import { CiMail } from "react-icons/ci";
 
 export function Footer() {
   const { theme } = useContext(ThemeContext);
   return (
     <footer className="text-gray-900 bg-white dark:bg-background-dark flex justify-center items-center h-20 p-4">
-      <div className="flex flex-col lg:flex-row justify-between items-center w-full max-w-screen-2xl px-4 lg:px-8">
-        <div className="text-xl lg:text-2xl xl:text-3xl font-bold text-teal-400 mb-4 lg:mb-0">
+      <div className="flex flex-col md:flex-row justify-between items-center w-full max-w-screen-xl">
+        <div className="text-xl md:text-2xl font-bold text-teal-400 mb-4 md:mb-0">
           <span>
             <Link href="/">Code House</Link>
           </span>
         </div>
-        <div className="text-center text-gray-800 dark:text-white mb-4 lg:mb-0">
+        <div className="text-center text-gray-800 dark:text-white mb-4 md:mb-0">
           <span>© 2024 Code House. Design & Desenvolvimento de Software.</span>
         </div>
-        <div className="flex text-gray-800 dark:text-white space-x-4 justify-center lg:justify-start">
+        <div className="flex text-gray-800 dark:text-white space-x-4">
           <span>
             <Link href="#">
               <FaCartShopping />
